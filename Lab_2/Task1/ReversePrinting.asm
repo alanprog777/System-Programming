8a598cfd5c6ee770a18a144e5cca5949c61e0bd2
alanprog777 <alan@DESKTOP-PPVR79R.localdomain>
2025-09-26 09:38:33 +0300

    done

---
+++ Lab_2/Task1/ReversePrinting.asm
@@ -0,0 +1,36 @@
+format ELF64
+public _start
+public exit
+
+section '.bss' writable
+symbols db 0xA, "AMVtdiYVETHnNhuYwnWDVBqL"
+
+section '.text' executable
+_start:
+  mov rcx, 52
+  .iter:
+    push rcx
+
+    mov rax, symbols
+    add rax, rcx
+    mov rcx, rax
+
+    mov rax, 4
+    mov rbx, 1
+    mov rdx, 1
+    int 0x80
+
+    pop rcx
+
+    dec rcx
+    cmp rcx, -1
+    jne .iter
+
+
+  call exit
+
+
+exit:
+  mov rax, 1
+  xor rbx, rbx
+  int 0x80
