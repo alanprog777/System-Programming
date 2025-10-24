8cc68e37d66366c4791d0da6e0604ad53dbe242e
alanprog777 <alan@DESKTOP-PPVR79R.localdomain>
2025-10-24 08:52:42 +0300

    done

--- Lab_2/Task4/Summ.asm
+++ Lab_2/Task4/Summ.asm
@@ -1,19 +1,94 @@
 format ELF64
 
 public _start
-public exit
 public print
 
-section '.data'
-  nl db 10
-
-section '.bss' writable
-  num dq 5277616985
+  num dq 568093600
+  ;str db 0xA
   res dq 0
   ten dq 10
   place db 1
 
+format ELF64
+
+public _start
+
+section '.data'
+    num dq 568093600
+    ten dq 10
+    newline db 0xA
+
+section '.bss'
+    res dq 0
+    place db 0
+
 section '.text' executable
+_start:
+    mov rax, [num]
+    xor rbx, rbx
+
+    .sum_loop:
+      xor rdx, rdx
+      div qword [ten]
+      add rbx, rdx
+      cmp rax, 0
+      jne .sum_loop
+
+    mov [res], rbx
+
+    call print
+
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, newline
+    mov rdx, 1
+    syscall
+
+    mov rax, 60
+    xor rdi, rdi
+    syscall
+
+print:
+    mov rax, [res]
+    xor rbx, rbx
+
+    cmp rax, 9
+    jle .single_digit
+
+    mov rcx, 10
+    .loop:
+        xor rdx, rdx
+        div rcx
+        push rdx
+        inc rbx
+        test rax, rax
+        jnz .loop
+
+    .print_loop:
+        pop rax
+        add al, '0'
+        mov [place], al
+
+        mov rax, 1
+        mov rdi, 1
+        mov rsi, place
+        mov rdx, 1
+        syscall
+
+        dec rbx
+        jnz .print_loop
+        ret
+
+    .single_digit:
+        add al, '0'
+        mov [place], al
+
+        mov rax, 1
+        mov rdi, 1
+        mov rsi, place
+        mov rdx, 1
+        syscall
+        ret
   _start:
     mov rax, [num]
     xor rbx, rbx
@@ -29,18 +104,12 @@ section '.text' executable
 
 
     call print
-    call newline
+
     mov eax, 60
     xor edi, edi
-    call exit
-
-newline:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, nl
-    mov rdx, 1
-    syscall
-    ret
+    mov eax, 1;exit
+    mov ebx, 0
+    int 0x80
 
 print:
     mov rax, [res]
@@ -84,8 +153,3 @@ print:
         mov edx, 1
         syscall
         ret
-
-exit:
-  mov eax, 1
-  mov ebx, 0
-  int 0x80
--- Lab_2/Task5/ShortSumm.asm
+++ Lab_2/Task5/ShortSumm.asm
@@ -1,96 +1,19 @@
 format ELF64
 
 public _start
+public exit
 public print
 
-  num dq 568093600
-  ;str db 0xA
+section '.data'
+  nl db 10
+
+section '.bss' writable
+  num dq 5277616985
   res dq 0
   ten dq 10
-  place db 1          ; Место для временного хранения символа для вывода
-
-format ELF64
-
-public _start
-
-section '.data'
-    num dq 568093600
-    ten dq 10
-    newline db 0xA
-
-section '.bss'
-    res dq 0
-    place db 0
+  place db 1
 
 section '.text' executable
-_start:
-    mov rax, [num]
-    xor rbx, rbx
-
-    .sum_loop:
-      xor rdx, rdx
-      div qword [ten]
-      add rbx, rdx
-      cmp rax, 0
-      jne .sum_loop
-
-    mov [res], rbx
-
-    call print
-
-    ; ДОБАВЛЕНО: вывод символа новой строки
-    mov rax, 1          ; sys_write
-    mov rdi, 1          ; stdout
-    mov rsi, newline    ; указатель на символ новой строки
-    mov rdx, 1          ; длина
-    syscall
-
-    ; Завершение программы
-    mov rax, 60         ; sys_exit
-    xor rdi, rdi        ; код возврата 0
-    syscall
-
-print:
-    mov rax, [res]
-    xor rbx, rbx
-
-    cmp rax, 9
-    jle .single_digit
-
-    mov rcx, 10
-    .loop:
-        xor rdx, rdx
-        div rcx
-        push rdx
-        inc rbx
-        test rax, rax
-        jnz .loop
-
-    .print_loop:
-        pop rax
-        add al, '0'     
-        mov [place], al
-
-        mov rax, 1      ; sys_write
-        mov rdi, 1      ; stdout
-        mov rsi, place  ; указатель на символ
-        mov rdx, 1      ; длина
-        syscall
-
-        dec rbx
-        jnz .print_loop
-        ret
-
-    .single_digit:
-        add al, '0'
-        mov [place], al
-
-        mov rax, 1      ; sys_write
-        mov rdi, 1      ; stdout
-        mov rsi, place  ; указатель на символ
-        mov rdx, 1      ; длина
-        syscall
-        ret
   _start:
     mov rax, [num]
     xor rbx, rbx
@@ -106,12 +29,18 @@ print:
 
 
     call print
-
+    call newline
     mov eax, 60
     xor edi, edi
-    mov eax, 1;exit
-    mov ebx, 0
-    int 0x80
+    call exit
+
+newline:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, nl
+    mov rdx, 1
+    syscall
+    ret
 
 print:
     mov rax, [res]
@@ -155,3 +84,8 @@ print:
         mov edx, 1
         syscall
         ret
+
+exit:
+  mov eax, 1
+  mov ebx, 0
+  int 0x80
