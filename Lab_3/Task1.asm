490d25f833268e31158541a5ffd96da2cdf59cc9
alanprog777 <alan@DESKTOP-PPVR79R.localdomain>
2025-11-03 19:37:34 +0300

    done

--- Lab_3/Task1.asm
+++ Lab_3/Task1.asm
@@ -1,87 +1,59 @@
 format ELF64
 public _start
+public exit
 
-section '.data' writable
-    msg db "ASCII code: "
-    msg_len = $ - msg
+section '.bss' writable
     newline db 10
-    buffer rb 4          ; буфер для вывода числа
+    place db 1
 
 section '.text' executable
 _start:
-    ; Проверяем количество аргументов
-    pop rcx              ; argc
-    cmp rcx, 2
-    jl exit              ; если меньше 2 аргументов - выходим (ИСПРАВЛЕНО: было exit_program)
-
-    ; Пропускаем имя программы
+    add rsp, 16
     pop rsi
+    mov byte al, [rsi]
 
-    pop rsi
+    call print
 
-    mov al, [rsi]        ; первый символ аргумента
-    cmp byte [rsi + 1], 0 ; проверяем что второй символ - конец строки
-    jne exit             ; если не конец строки - выходим (ИСПРАВЛЕНО: было exit_program)
-
-    mov rax, 1           ; sys_write
-    mov rdi, 1           ; stdout
-    mov rsi, msg
-    mov rdx, msg_len
-    syscall
-
-    movzx rax, al        ; расширяем AL до RAX (наш символ)
-    mov rdi, buffer      ; буфер для результата
-    call number_to_string
-
-    mov rax, 1           ; sys_write
-    mov rdi, 1           ; stdout
-    mov rsi, buffer
-    mov rdx, 3           ; максимальная длина числа (3 цифры)
-    syscall
-
-    mov rax, 1           ; sys_write
-    mov rdi, 1           ; stdout
+    mov rax, 1
+    mov rdi, 1
     mov rsi, newline
     mov rdx, 1
     syscall
 
-    jmp exit
+    call exit
+
+print:
+    xor rbx, rbx
+
+    mov rcx, 10
+    .loop:
+        xor rdx, rdx
+        div rcx
+        push rdx
+        inc rbx
+        test rax, rax
+        jnz .loop
+
+    .print_loop:
+        pop rax
+        add rax, '0'
+        mov [place], al
+
+
+        push rbx
+        mov rax, 1
+        mov rdi, 1
+        mov rsi, place
+        mov rdx, 1
+        syscall
+        pop rbx
+
+        dec rbx
+        jnz .print_loop
+
+        ret
 
 exit:
-    mov rax, 60          ; sys_exit
-    xor rdi, rdi         ; код возврата 0
+    mov rax, 60
+    xor rdi, rdi
     syscall
-
-number_to_string:
-    push rbx
-    push rcx
-    push rdx
-
-    mov rbx, rdi         ; сохраняем начало буфера
-    add rdi, 3           ; перемещаемся к концу буфера
-    mov byte [rdi], 0    ; завершающий нуль
-    dec rdi
-
-    mov rcx, 10          ; основание системы счисления
-
-.convert_loop:
-    xor rdx, rdx
-    div rcx              ; RAX = частное, RDX = остаток
-    add dl, '0'          ; преобразуем цифру в символ
-    mov [rdi], dl
-    dec rdi
-    test rax, rax
-    jnz .convert_loop
-
-.fill_spaces:
-    cmp rdi, rbx
-    jb .done
-    mov byte [rdi], ' '
-    dec rdi
-    jmp .fill_spaces
-
-.done:
-    pop rdx
-    pop rcx
-    pop rbx
-    ret
